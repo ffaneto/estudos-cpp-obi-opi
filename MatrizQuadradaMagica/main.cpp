fb2907abbb463c4f12148bb33dad32fe4b6d9591
ffaneto <misteryshadbr@gmail.com>
2026-05-15 13:45:38 -0300

    Adição de Mais Questões

---
+++ MatrizQuadradaMagica/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int m[3][3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> m[i][j];
+        }
+        
+    }
+
+   int ref = m[0][0] + m[0][1] + m[0][2];
+   bool resultado = true;
+
+    if((m[1][0] + m[1][1] + m[1][2]) != ref ) resultado = false;
+    if((m[2][0] + m[2][1] + m[2][2]) != ref ) resultado = false;
+
+    if ((m[0][0] + m[1][0] + m[2][0]) != ref) resultado = false;
+    if ((m[0][1] + m[1][1] + m[2][1]) != ref) resultado = false;
+    if ((m[0][2] + m[1][2] + m[2][2]) != ref) resultado = false;
+
+    if((m[0][0] + m[1][1] + m[2][2] != ref)) resultado = false;
+    if((m[0][2] + m[1][1] + m[2][0] != ref)) resultado = false;
+
+
+    if (resultado) {
+        cout << "SIM" << endl;
+    } else {
+        cout << "NAO" << endl;
+    }
+
+
+}
